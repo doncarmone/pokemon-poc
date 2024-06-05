fa78796acf047f0c9d46e4b97a9fdc0972a05ec7
Javier Carmona <javier.carmona@wizeline.com>
2024-06-05 09:36:30 -0600

    [ADD] Added the first components

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+            class="brightness-0" />
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+img {
+    width: 200px;
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+}
+</style>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,16 @@
+<template>
+    <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl">Espere</h1>
+        <h3 class="animate-pulse">Cargando pokemons</h3>
+    </section>
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="mt-5">Quien es este pokemon?</h1>
+        <PokemonPicture />
+        <PokemonOptions />
+    </section>
+</template>
+
+<script setup lang="ts">
+import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue'
+import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue'
+</script>
